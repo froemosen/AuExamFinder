67f999dd04feb8225c1cc20962d39248b63fee85
Kristian Anton Hedegaard <kristianantonhedegaard@gmail.com>
2025-11-04 08:51:50 +0100

    Some small style changes

--- frontend/static/templates.js
+++ frontend/static/templates.js
@@ -1,4 +1,4 @@
-const cookiesPopup = ` <div class="notification is-info is-light" style="position: fixed; top: 85%; left: 75%; transform: translate(-50%, -50%); z-index: 1000; max-width: 400px;">
+const cookiesPopup = ` <div class="notification is-info is-light cookies-popup">
   <button class="delete" onclick="this.parentElement.remove()"></button>
   This website does not use cookies. We do however LOVE to eat them! 🍪🍪
 </div> `;
